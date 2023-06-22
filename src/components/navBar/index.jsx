69769ce77da9fbb1bbc4df4cd89b6f7e53e69f52
Camila Las Heras <camila.westnet@gmail.com>
2023-06-22 20:57:17 +0000

    Correccion

---
+++ src/components/navBar/index.jsx
@@ -0,0 +1,12 @@
+import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
+const NavBar = () => {
+    return (
+        <div>
+            <div style={{ backgroundColor: "grey", fontFamily: "system-ui" }}>
+                <h1>Mindfully Speaking</h1>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
